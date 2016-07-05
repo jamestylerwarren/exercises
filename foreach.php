944bafef74c44dad74c723070a711a182a71090a
Tyler Warren <warrejt@yahoo.com>
2016-07-05 14:57:19 -0500

    foreach done

--- foreach.php
+++ foreach.php
@@ -5,8 +5,13 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 foreach($things as $value) {
 	if (is_scalar($value)) {
 		echo $value . PHP_EOL;
+	} elseif (is_array($value)) {
+		foreach ($value as $otherThings) {
+			echo $otherThings . PHP_EOL;
+		}
 	}
 }
+
 
 
 
