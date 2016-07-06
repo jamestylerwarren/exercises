21c34fbf96f15c0e601afadcfd0316f45bdb6771
Tyler Warren <warrejt@yahoo.com>
2016-07-06 10:06:54 -0500

    break_continue updated

--- break_continue.php
+++ break_continue.php
@@ -1,7 +1,14 @@
 <?php
 
+// foreach (range(1, 100) as $i) {
+//     if ($i % 2 == 0) {
+//     	echo $i . "\n";
+//         continue;
+//     }
+// }
+
 foreach (range(1, 100) as $i) {
-    echo $i . "\n";
+	echo $i . "\n";
     if ($i == 10) {
         break;
     }
