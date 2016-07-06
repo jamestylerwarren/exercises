5fca3cf954254e269ea3385e083d08991eec978c
Tyler Warren <warrejt@yahoo.com>
2016-07-06 11:28:12 -0500

    arithmetic exercise #2

--- arithmetic.php
+++ arithmetic.php
@@ -20,7 +20,12 @@ function divide($a, $b)
     return $a / $b;
 }
 
+function modulus($a, $b) {
+	return $a % $b;
+}
+
 echo add(10, 2) . PHP_EOL;
 echo subtract(10, 2) . PHP_EOL;
 echo multiply(10, 2) . PHP_EOL;
 echo divide(10, 2) . PHP_EOL;
+echo modulus(10, 2) . PHP_EOL;
