14055fafee49250c8165f3bb111f02ba8f3a58ed
Tyler Warren <warrejt@yahoo.com>
2016-07-07 14:57:03 -0500

    exercise 2 and 3 done

--- internal_functions.php
+++ internal_functions.php
@@ -3,8 +3,9 @@
 // TODO: Create your inspect() function here
 
 function inspect($a) {
-	gettype($a) = $type;
-	intval($a) = $value;
+	$type = gettype($a); 
+	$value = intval($a); 
+	return "The $type is $value\n";
 }
 
 // Do not mofify these variables!
@@ -22,25 +23,24 @@ $null = NULL;
 
 // TODO: After each echo statement, use inspect() to output the variable's type and its value
 
-echo 'Inspecting $num1:' . PHP_EOL;
-	
+echo 'Inspecting $num1: ' . inspect($num1) . PHP_EOL;
 
-echo 'Inspecting $num2:' . PHP_EOL;
+echo 'Inspecting $num2:' . inspect($num2) . PHP_EOL;
 
-echo 'Inspecting $num3:' . PHP_EOL;
+echo 'Inspecting $num3:' . inspect($num3) . PHP_EOL;
 
-echo 'Inspecting $num4:' . PHP_EOL;
+echo 'Inspecting $num4:' . inspect($num4) . PHP_EOL;
 
-echo 'Inspecting $null:' . PHP_EOL;
+echo 'Inspecting $null:' . inspect($null) . PHP_EOL;
 
-echo 'Inspecting $bool1' . PHP_EOL;
+echo 'Inspecting $bool1' . inspect($bool1) . PHP_EOL;
 
-echo 'Inspecting $bool2' . PHP_EOL;
+echo 'Inspecting $bool2' . inspect($bool2) . PHP_EOL;
 
-echo 'Inspecting $string1' . PHP_EOL;
+echo 'Inspecting $string1' . inspect($string1) . PHP_EOL;
 
-echo 'Inspecting $string2' . PHP_EOL;
+echo 'Inspecting $string2' . inspect($string2) . PHP_EOL;
 
-echo 'Inspecting $array1' . PHP_EOL;
+echo 'Inspecting $array1' . inspect($array1) . PHP_EOL;
 
-echo 'Inspecting $array2' . PHP_EOL;
+echo 'Inspecting $array2' . inspect($array2) . PHP_EOL;
