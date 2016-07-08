47ec0559a46396dac78c768363d01b62e12e58fa
Tyler Warren <warrejt@yahoo.com>
2016-07-08 14:20:08 -0500

    exercise 1 on arrar search fully completed

--- search-arrays.php
+++ search-arrays.php
@@ -4,5 +4,24 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-$result = array_search('Dana', $names);
-var_dump($result);
+$query = 'Tina';
+
+function arraySearch($query, $names) {
+	if (array_search($query, $names) !== False) {
+		echo "true\n";
+	} else {
+		echo "false\n";
+	}
+} arraySearch($query, $names);
+
+
+
+
+ // if (array_search($query, $names)) {
+ // 	var_dump("True");
+ // } else {
+ // 	var_dump("False");
+ // }
+	
+
+
