28265519eac2af1e6709d4441355475ce4ad7bf2
Tyler Warren <warrejt@yahoo.com>
2016-07-11 11:14:40 -0500

    lis_with_commas exercise 2 done

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,9 +3,27 @@
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 $physicistsArray = explode(', ', $physicistsString);
-$removedArray = array_pop($physicistsArray);
-$tonyStark = 'and Tony Stark';
-$addedArray = array_push($physicistsArray, $tonyStark);
-$famousFakePhysicists = implode(', ', $physicistsArray);
+
+// $removedArray = array_pop($physicistsArray);
+// $andTonyStark = 'and Tony Stark';
+// $addedArray = array_push($physicistsArray, $andTonyStark);
+// $famousFakePhysicists = implode(', ', $physicistsArray);
+
+// echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+
+
+function humanizedList($array, $alphabatize = false) {
+	if ($alphabatize) {
+		sort($array);
+	}
+	$lastItem = array_pop($array);
+	$lastItemString = 'and ' . $lastItem;
+	array_push($array, $lastItemString);
+	$arrayToString = implode(', ', $array);
+	return ($arrayToString);	
+} 
+
+$famousFakePhysicists = humanizedList($physicistsArray, true);
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+
