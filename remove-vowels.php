37f1aa68f6eaa1749d823a5ee7991cbb3ca01487
Tyler Warren <warrejt@yahoo.com>
2016-07-14 09:45:41 -0500

    removed values from an array using a function that unsets the key of that particular value

---
+++ remove-vowels.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+
+function removeVowels(&$array) {
+	foreach ($array as $key => $letter) {
+		if ($letter == 'a' || $letter == 'e' || $letter == 'i' || $letter == 'o' || $letter == 'u') {
+			unset($array[$key]);
+		}
+	}
+	return $array;
+}
+var_dump(removeVowels($array));
