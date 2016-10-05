6161bd9b2210f3a1d7842f25d133f14a64c4b988
Tyler Warren <warrejt@yahoo.com>
2016-10-05 18:19:32 -0500

    bet and bankroll working;
    still need to trigger reload option when bankroll = $0;

--- blackjackgame.php
+++ blackjackgame.php
@@ -14,11 +14,13 @@ $player = [];
 fwrite(STDOUT, "Enter name: ") . PHP_EOL;
 $name = ucfirst(trim(fgets(STDIN)));
 
+// determine player bankroll
 do {
 	fwrite(STDOUT, "Enter your bankroll (table limit = $1,000,000): ") . PHP_EOL;
 	$bankroll = trim(fgets(STDIN));
 } while ($bankroll <= 0 || $bankroll > 1000000 || !is_numeric($bankroll));
 
+// determine bet size
 do {
 	fwrite(STDOUT, "Enter bet: ") . PHP_EOL;
 	$bet = trim(fgets(STDIN));
@@ -106,11 +108,17 @@ function echoDealer(&$dealer, $hidden = false) {
 	}
 }
 
-function playAgain($name) {
+function playAgain($name, $bankroll, $bet) {
 	fwrite(STDOUT, "Do you want to play again " . $name . "? (y)es or (n)o? ") . PHP_EOL;
 	$choice = trim(fgets(STDIN));
 	while ($choice == 'y') {
 		echo "Okay, shuffle up and deal!" . PHP_EOL;
+		echo "Bankroll = $" . $bankroll . '.' . PHP_EOL;
+		//determine bet size
+		do {
+			fwrite(STDOUT, "Enter bet: ") . PHP_EOL;
+			$bet = trim(fgets(STDIN));
+		} while ($bet <= 0 || $bet > $bankroll || !is_numeric($bet));
 		// create an array for cards
 		$suits = ['C', 'H', 'S', 'D'];
 		$cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
@@ -143,20 +151,25 @@ function playAgain($name) {
 					}
 					//notify when dealer busts
 					if (getTotal($dealer) > 21) {
-						echo 'Dealer busted! ' . $name . ' wins!' . PHP_EOL;
-						playAgain($name);
+						$bankroll += $bet;
+						echo 'Dealer busted! ' . $name . ' wins $' . $bet . '!' . PHP_EOL;
+						echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
+						playAgain($name, $bankroll, $bet);
 				}
 				//Evaluate Hands
 				if (getTotal($player) == getTotal($dealer)) {
-					echo $name . ' and Dealer push!' . PHP_EOL;
-				}
-				if (getTotal($player) > getTotal($dealer)) {
+					echo $name . ' and Dealer push! Bankroll still at ' . $bankroll . '.' . PHP_EOL;
+				} elseif (getTotal($player) > getTotal($dealer)) {
+					$bankroll += $bet;
 					echo $name . ' wins!' . PHP_EOL;
+					echo $name . ' wins ' . $bet . '!' . PHP_EOL;
+					echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
+				} elseif (getTotal($player) < getTotal($dealer)) {
+					$bankroll -= $bet;
+					echo 'Dealer wins! ' . $name . ' loses ' . $bet . '!' . PHP_EOL;
+					echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
 				}
-				if (getTotal($player) < getTotal($dealer)) {
-					echo 'Dealer wins! ' . $name . ' loses.' . PHP_EOL;
-				}
-				playAgain($name);
+				playAgain($name, $bankroll, $bet);
 
 			//Hit option
 			} elseif ($decision == 'h') {
@@ -168,20 +181,18 @@ function playAgain($name) {
 					echo '[' . $card['card'] . ' ' . $card['suit'] . '] ';
 				}
 				echo $name . ' total = ' . $total . PHP_EOL;
-				//notify when player hits 21
-				if (getTotal($player) == 21) {
-					echo 'Blackjack!! ' . $name . ' wins!' . PHP_EOL;
-					playAgain($name);
-				}
 				//notify when player busts
 				if (getTotal($player) > 21) {
+					$bankroll -= $bet;
 					echo $name . ' busted! Dealer wins.' . PHP_EOL;
-					playAgain($name);
+					echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
+					playAgain($name, $bankroll, $bet);
 				}
 			}
 		}
 		echo 'Blackjack!! ' . $name . ' wins!' . PHP_EOL;
-		playAgain($name);
+		echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
+		playAgain($name, $bankroll, $bet);
 	}
 	echo "Ok, thanks for playing " . $name . "!" . PHP_EOL;
 	exit();
@@ -212,20 +223,25 @@ while (getTotal($player) < 21) {
 			}
 			//notify when dealer busts
 			if (getTotal($dealer) > 21) {
-				echo 'Dealer busted! ' . $name . ' wins!' . PHP_EOL;
-				playAgain($name);
+				$bankroll += $bet;
+				echo 'Dealer busted! ' . $name . ' wins $' . $bet . '!' . PHP_EOL;
+				echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
+				playAgain($name, $bankroll, $bet);
 		}
 		//Evaluate Hands
 		if (getTotal($player) == getTotal($dealer)) {
-			echo $name . ' and Dealer push!' . PHP_EOL;
-		}
-		if (getTotal($player) > getTotal($dealer)) {
+			echo $name . ' and Dealer push! Bankroll still at ' . $bankroll . '.' . PHP_EOL;
+		} elseif (getTotal($player) > getTotal($dealer)) {
+			$bankroll += $bet;
 			echo $name . ' wins!' . PHP_EOL;
+			echo $name . ' wins ' . $bet . '!' . PHP_EOL;
+			echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
+		} elseif (getTotal($player) < getTotal($dealer)) {
+			$bankroll -= $bet;
+			echo 'Dealer wins! ' . $name . ' loses ' . $bet . '!' . PHP_EOL;
+			echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
 		}
-		if (getTotal($player) < getTotal($dealer)) {
-			echo 'Dealer wins! ' . $name . ' loses.' . PHP_EOL;
-		}
-		playAgain($name);
+		playAgain($name, $bankroll, $bet);
 
 	//Hit option
 	} elseif ($decision == 'h') {
@@ -237,20 +253,18 @@ while (getTotal($player) < 21) {
 			echo '[' . $card['card'] . ' ' . $card['suit'] . '] ';
 		}
 		echo $name . ' total = ' . $total . PHP_EOL;
-		//notify when player hits 21
-		if (getTotal($player) == 21) {
-			echo 'Blackjack!! ' . $name . ' wins!' . PHP_EOL;
-			playAgain($name);
-		}
 		//notify when player busts
 		if (getTotal($player) > 21) {
+			$bankroll -= $bet;
 			echo $name . ' busted! Dealer wins.' . PHP_EOL;
-			playAgain($name);
+			echo 'Bankroll = $' . $bankroll . '.' . PHP_EOL;
+			playAgain($name, $bankroll, $bet);
 		}
 	}
 }
-echo 'Blackjack!! ' . $name . ' wins!' . PHP_EOL;
-playAgain($name);
+$bankroll += ($bet * 1.25);
+echo 'Blackjack!! You have $' . $bankroll . ' behind you!' . PHP_EOL;
+playAgain($name, $bankroll, $bet);
 
 
 
