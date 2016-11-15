6111b6ddca440f38a6361c34e82751dfe05c6331
Tyler Warren <warrejt@yahoo.com>
2016-11-15 15:57:54 -0600

    just finished benefactor problem

--- code_wars.php
+++ code_wars.php
@@ -19,18 +19,23 @@
 // median([10, 29, 23, 94, 76, 96, 5, 85, 4, 33, 47, 41, 87]);
 
 
-function compose($s1, $s2) {
-    //explode strings into arrays
-    $s1 = explode("\n", $s1);
-    $s2 = explode("\n", $s2);
-    
-    var_dump($s1);
+// Looking for a benefactor
+// function newAvg($arr, $newavg) {
+//     $n = count($arr);
+//     $new_tot = $newavg * ($n + 1);
+//     $curr_tot = 0;
+//     $i = 0;
+//     while ($i < $n) {
+//       $curr_tot += $arr[$i];
+//       $i++;
+//     }
+//     if ($curr_tot < $new_tot) {
+//       return ceil($new_tot - $curr_tot);
+//     } else {
+//       throw new InvalidArgumentException();
+//     }
+// }
+// var_dump(newAvg([14, 30, 5, 7, 9, 11, 15], 92));
 
 
 
-    //last step is imploding all array keys into a string;
-}
-
-$s1 = "byGt\nhTts\nRTFF\nCnnI";
-$s2 = "jIRl\nViBu\nrWOb\nNkTB";
-compose($s1, $s2);
